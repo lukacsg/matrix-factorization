ed6ad7920b65d04a78ea74cadee1099eb0fcd680
bereczdaniel <berecz.17@gmail.com>
2019-05-30 15:03:51 +0200

    SGD update

---
+++ src/main/scala/optimizer/Optimizer.scala
@@ -0,0 +1,24 @@
+package optimizer
+
+/**
+  * Holds a function that computes delta updates based on a rating to the corresponding
+  * user and an item vectors.
+  *
+  * E.g. it can describe an SGD update ([[SGD]]).
+  */
+trait Optimizer extends Serializable {
+
+      /**
+    * @param rating
+    * Rating.
+    * @param user
+    * User vector.
+    * @param item
+    * Item vector.
+    * @return
+    * Vector to add to user and item vector respectively.
+    */
+  def delta(rating: Double,
+            user: Array[Double],
+            item: Array[Double]): (Array[Double], Array[Double])
+}
---
+++ src/main/scala/optimizer/SGD.scala
@@ -0,0 +1,11 @@
+package optimizer
+
+class SGD(learningRate: Double) extends Optimizer {
+
+    override def delta(rating: Double, user: Array[Double], item: Array[Double]): (Array[Double], Array[Double]) = {
+      val e = rating - user.zip(item).map { case (x, y) => x * y }.sum
+
+      (item.map(i => learningRate * e * i),
+       user.map(u => learningRate * e * u))
+  }
+}
